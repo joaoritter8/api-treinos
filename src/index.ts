44e58e78c4044ea0a32087c9d1979b1fc390ce7f
joaoritter8 <joaoritter8@gmail.com>
2026-03-03 21:09:01 -0400

    chore: add fastify swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import Fastify from "fastify";
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -11,10 +14,36 @@ const app = Fastify({
   logger: true,
 });
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Treinos API",
+      description: "API para gerenciamento de treinos",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        description: "localhost",
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  transform: jsonSchemaTransform,
+});
+
+await app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
 app.withTypeProvider<ZodTypeProvider>().route({
   method: "GET",
   url: "/",
   schema: {
+    description: "Hello World",
+    tags: ["Hello"],
     response: {
       200: z.object({
         message: z.string(),
@@ -27,9 +56,6 @@ app.withTypeProvider<ZodTypeProvider>().route({
 });
 
 app.listen({ port: Number(process.env.PORT) });
-
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
 
 try {
   await app.listen({ port: Number(process.env.PORT) });
